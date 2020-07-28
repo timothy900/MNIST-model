bc17ed4eaf912949abbc76b5a3b3d201131934c9
Timothy <47323731+timothy900@users.noreply.github.com>
2020-07-29 07:22:09 +0800

    Version 1.0
    Everything is completed and implemented.

--- digitsClassify.py
+++ digitsClassify.py
@@ -67,12 +67,16 @@ class Canvas:
 		pygame.draw.line(screen, white, (canvas_width, 0), (canvas_width, canvas_width), 3)
 
 
-# button for clearing canvas
-class Button:
-	def __init__(self, pos, size, text):
+# deals with all the UI elements(not including the canvas)
+class UI:
+	def __init__(self, pos, size, text, canvas):
 		self.pos = pos
 		self.size = size 
 		self.text = text
+		# for predictions
+		self.canvas = canvas
+		self.pred = None
+		self.first = True
 
 	# return true if mouse is over the button
 	def isOn(self):
@@ -84,29 +88,46 @@ class Button:
 				return True
 		return False
 
-	# draw the button
-	def draw(self):
-		if self.isOn(): col = (200, 200, 200)
-		else: col = (255, 255, 255)
-		
+	# draw the UI
+	def draw(self, canvas, do_pred):
+		# header
 		x, y = self.pos[0], self.pos[1]
 		w, h = self.size[0], self.size[1]
 		
-		text = font.render(self.text, True, bg_color, col) 
-		text_rect = text.get_rect() 
-		text_rect.center = (x + (w // 2), y + (h // 2)) 
+		head_text = font.render("Draw a single digit", True, white) 
+		head_text_rect = head_text.get_rect() 
+		head_text_rect.center = (x + (w // 2), y-50 )#+ (h // 2)) 
+
+		screen.blit(head_text, head_text_rect) 
+
+		# reset button
+		if self.isOn(): col = (200, 200, 200)
+		else: col = (255, 255, 255)
+
+		button_text = font.render(self.text, True, bg_color, col) 
+		button_text_rect = button_text.get_rect() 
+		button_text_rect.center = (x + (w // 2), y + (h // 2)) 
 
 		pygame.draw.rect(screen, col, (x, y, w, h))
-		screen.blit(text, text_rect) 
+		screen.blit(button_text, button_text_rect) 
+
+		# predictions		 
+		if canvas.cells.any() and do_pred:
+			self.pred = int(model.predict(canvas.cells.reshape(1, -1)))
+			self.first = False
+		else: 
+			if self.first: self.pred = "None"
+
+		pred_text = font.render("Prediction: " + str(self.pred), True, white) 
+		pred_text_rect = pred_text.get_rect() 
+		pred_text_rect.center = (x + (w // 2), 2*y + (h // 2))
+
+		screen.blit(pred_text, pred_text_rect) 
 
 	def pressed(self, canvas):
 		if self.isOn():
 			canvas.cells = np.zeros((canvas.res, canvas.res))
-
-
-class UI:
-	def __init__(self):
-		pass
+			self.pred = "None"
 
 
 def main():
@@ -115,32 +136,33 @@ def main():
 
 	canvas = Canvas()
 	canvas_width = (canvas.res * canvas.cell_size)
-	reset_button_pos = ((canvas_width + (width - canvas_width)//2) - 150 , 100)
-	reset_button = Button(reset_button_pos, (300, 100), "Reset Canvas")
+	reset_button_pos = ((canvas_width + (width - canvas_width)//2) - 150 , 130)
+	ui = UI(reset_button_pos, (300, 100), "Reset Canvas", canvas)
 
 	i = 0
+	do_pred = False
 	while run:
 		clock.tick(60)
 
 		i += 1
-		# predict once every 60 frames
-		if i == 60:
-			print(model.predict(canvas.cells.reshape(1, -1)))
-			# print(canvas.cells.reshape(1,-1))
+		# predict once every 20 frames
+		if i == 20:
+			do_pred = True
 			i = 0
+		else: do_pred = False
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: 
 				run = False
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				canvas.drawing = True
-				reset_button.pressed(canvas)
+				ui.pressed(canvas)
 			if event.type == pygame.MOUSEBUTTONUP:
 				canvas.drawing = False
 
 		screen.fill(bg_color)
 		canvas.draw()
-		reset_button.draw()
+		ui.draw(canvas, do_pred)
 
 		pygame.display.flip()
 
